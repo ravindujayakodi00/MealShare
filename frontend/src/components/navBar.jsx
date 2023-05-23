f2019b6f48c35f8bf3b6881f9b5eff8b0a067443
ravindujayakodi00 <ravindujayakodi00@gmail.com>
2023-05-23 19:51:47 +0530

    Update Navbar

--- frontend/src/components/navBar.jsx
+++ frontend/src/components/navBar.jsx
@@ -1,15 +1,24 @@
-import { useState,React } from "react";
+import { useState, React } from "react";
 import { Close, Menu, Navbar0 } from "../assets";
 import { navLinks } from "../constants";
+import { Link } from "react-router-dom";
+import { useAuthContext } from "../hooks/useAuthContext";
+import { useLogout } from "../hooks/useLogout";
 
 const Navbar = () => {
   const [toggle, setToggle] = useState(false);
   const [loggedIn, setLoggedIn] = useState(false);
   const [userAvatar, setUserAvatar] = useState(null);
+  const { user, logout } = useAuthContext();
+  const { logout: handleLogout } = useLogout();
 
   const handleLogin = () => {
     setLoggedIn(true);
     setUserAvatar("url-of-user-avatar-image");
+  };
+
+  const handleLogoutClick = () => {
+    handleLogout();
   };
 
   return (
@@ -23,96 +32,39 @@ const Navbar = () => {
         {navLinks.map((nav, index) => (
           <li
             key={nav.id}
-            className={`font-poppins font-normal cursor-pointer text-[16px] ${
+            className={`mr-8 font-poppins font-normal cursor-pointer text-[16px] ${
               index === navLinks.length - 1 ? "mr-0" : "mr-10"
             } text-white`}
           >
             <a href={nav.path}>{nav.title}</a>
           </li>
         ))}
-        {!loggedIn && (
+        {user ? (
+          <div className="flex">
+     
+            <button
+              onClick={() => handleLogoutClick()}
+              className="bg-blue-500 text-white px-4 py-2 rounded-full"
+            >
+              Logout
+            </button>
+          </div>
+        ) : (
           <>
-            <li className="ml-6">
-              <button onClick={() => handleLogin()} className="mr-2">
-                Sign In
-              </button>
-            </li>
-            <li className="ml-3">
-              <button
-                onClick={() => handleLogin()}
-                className="bg-blue-500 text-white px-4 py-2 rounded-full"
-              >
-                Sign Up
-              </button>
-            </li>
+            <Link to="login" onClick={() => handleLogin()} className="mr-6">
+              Sign In
+            </Link>
+            <Link
+              to = "/signup"
+              onClick={() => handleLogin()}
+              className="bg-blue-500 text-white px-4 py-2 rounded-full mr-4"
+            >
+              Sign Up
+            </Link>
           </>
         )}
       </ul>
 
-      <div className="sm:hidden flex flex-1 justify-end items-center">
-        {loggedIn ? (
-          <img
-            src={userAvatar}
-            alt="user-avatar"
-            className="w-[28px] h-[40px] object-contain mb-8 rounded-full"
-          />
-        ) : (
-          <>
-            <button onClick={() => handleLogin()} className="mr-2">
-              Sign In
-            </button>
-            <button
-              onClick={() => handleLogin()}
-              className="bg-blue-500 text-white px-4 py-2 rounded-full"
-            >
-              Sign Up
-            </button>
-          </>
-        )}
-
-        <img
-          src={toggle ? Close : Menu}
-          alt="menu"
-          className="w-[28px] h-[40px] object-contain mb-8"
-          onClick={() => setToggle((prev) => !prev)}
-        />
-
-        <div
-          className={`${
-            toggle ? "flex" : "hidden"
-          } p-6 bg-black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] rounded-xl sidebar`}
-        >
-          <ul className="list-none flex flex-col justify-end items-center flex-1 mb-5">
-            {navLinks.map((nav, index) => (
-              <li
-                key={nav.id}
-                className={`font-poppins font-normal cursor-pointer text-[16px] ${
-                  index === navLinks.length - 1 ? "mb-0" : "mb-4"
-                } text-white`}
-              >
-                <a href={nav.path}>{nav.title}</a>
-              </li>
-            ))}
-            {!loggedIn && (
-              <>
-                <li>
-                  <button onClick={() => handleLogin()} className="mr-2">
-                    Sign In
-                  </button>
-                </li>
-                <li>
-                  <button
-                    onClick={() => handleLogin()}
-                    className="bg-blue-500 text-white px-4 py-2 rounded-full"
-                  >
-                    Sign Up
-                  </button>
-                </li>
-              </>
-            )}
-          </ul>
-        </div>
-      </div>
     </nav>
   );
 };
