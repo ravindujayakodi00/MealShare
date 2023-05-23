fb9675a0d96469404867f9ecadd78a7bd73ca9ea
ravindujayakodi00 <ravindujayakodi00@gmail.com>
2023-05-23 19:53:52 +0530

    Update style properties

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,9 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import { AuthContextProvider } from './context/AuthContext'
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <AuthContextProvider>
     <App />
+    </AuthContextProvider>
   </React.StrictMode>,
 )
+
+document.querySelector('body').classList.add('bg-gradient-to-r', 'from-green-500', 'via-green-300', 'to-yellow-300', 'w-full')
